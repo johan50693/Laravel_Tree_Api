0d01a826701087b667efebaa6f60ade0e77faf76
Albert Urbina <johan50693@gmail.com>
2026-02-03 20:20:23 -0400

    chore: initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use OpenApi\Annotations as OA;
+
+/*
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Tree API",
+ *      description="Tree Nodes Management API",
+ *      @OA\Contact(
+ *          email="admin@example.com"
+ *      )
+ * )
+ */
+abstract class Controller
+{
+    //
+}
